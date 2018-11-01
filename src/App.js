6ef72087b87c4fec5b2e1288ba1cfeae3123e18c
terefs <d.van.zurk@gmail.com>
2018-11-01 15:15:28 +0100

    installed sass

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AddScore from './containers/addscore';
 import VisibleTodoList from './containers/visiblescorelist'
 import Footer from './components/footer';
+import './sass/app.scss';
 
 
 const App = () => (
